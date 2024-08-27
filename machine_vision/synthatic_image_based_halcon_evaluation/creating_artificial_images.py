2a64e1124e7871fe0bee47a0527250686d6ea15a
malithjkd <malithjayawardhana@gmail.com>
2024-08-27 17:30:31 +0800

    creating image

--- machine_vision/synthatic_image_based_halcon_evaluation/creating_artificial_images.py
+++ machine_vision/synthatic_image_based_halcon_evaluation/creating_artificial_images.py
@@ -2,6 +2,8 @@ import numpy as np
 from PIL import Image
 from PIL import Image, ImageDraw
 
+
+
 # Create a sample NumPy array
 # data = np.random.randint(0, 255, (100, 100, 3))  # color images
 data = np.random.randint(0, 255, (100, 100))  # Black and white image
@@ -14,12 +16,12 @@ print(data.shape)
 image = Image.fromarray(data)
 
 # Save as BMP
-image.save('output.bmp')
+#image.save('output.bmp')
 
 
 
 # Create a new image
-image2 = Image.new('RGB', (2000, 2000), (255, 255, 255))  # White background
+image2 = Image.new('RGB', (2592, 2048), (255, 255, 255))  # White background
 
 # Create a drawing context
 draw = ImageDraw.Draw(image2)
@@ -31,12 +33,16 @@ radius = 50  # Radius
 
 # Draw a black circle
 #draw.ellipse(center1 + center2, fill='black', outline='black', width=1)
-draw.ellipse([800,800,1200,1200], fill=(0,0,0), outline=(20,20,20), width=10)
+draw.ellipse([800,800,1200,1200], fill=(0,0,0), outline=(20,20,20), width=0)
 
-#draw.circle(center, radius, fill='black')
+# Convert to grayscale (black and white)
+image2 = image2.convert('L')
+
+# Resize the image to 260x200 pixels
+new_image = image2.resize((260, 200), Image.ANTIALIAS)
 
 # save image
-image2.show()
-image2.save('circle_image.bmp')
+new_image.show()
+new_image.save('circle_image.bmp')
 
 
