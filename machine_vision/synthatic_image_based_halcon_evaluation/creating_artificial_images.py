80f92dba61f5eef09b0bab40506784d06e6c8f5c
malithjkd <malithjayawardhana@gmail.com>
2024-08-28 09:08:36 +0800

    create high resolution image and compress it 475 time

--- machine_vision/synthatic_image_based_halcon_evaluation/creating_artificial_images.py
+++ machine_vision/synthatic_image_based_halcon_evaluation/creating_artificial_images.py
@@ -6,40 +6,34 @@ from PIL import Image, ImageDraw
 
 # Create a sample NumPy array
 # data = np.random.randint(0, 255, (100, 100, 3))  # color images
-data = np.random.randint(0, 255, (100, 100))  # Black and white image
+# data = np.random.randint(0, 255, (100, 100))  # Black and white image
 
 # Convert data type to uint8 (optional, if needed)
-data = data.astype(np.uint8)  
+# data = data.astype(np.uint8)  
 
-print(data.shape)
+# print(data.shape)
 # Convert to PIL Image
-image = Image.fromarray(data)
+# image = Image.fromarray(data)
 
 # Save as BMP
 #image.save('output.bmp')
 
-
-
 # Create a new image
-image2 = Image.new('RGB', (2592, 2048), (255, 255, 255))  # White background
+# image =Image.nre('RGB', (column, rows),)
+image2 = Image.new('RGB', (12312, 9728), (255, 255, 255))  # White background
 
 # Create a drawing context
 draw = ImageDraw.Draw(image2)
 
-# Define the center and radius of the circle
-center1 = (500,500)  # Center coordinates
-center2 = (1000,1000)
-radius = 50  # Radius
-
 # Draw a black circle
 #draw.ellipse(center1 + center2, fill='black', outline='black', width=1)
-draw.ellipse([800,800,1200,1200], fill=(0,0,0), outline=(20,20,20), width=0)
+draw.ellipse([3656,2364,8656,7364], fill=(0,0,0), outline=(20,20,20), width=0)
 
 # Convert to grayscale (black and white)
 image2 = image2.convert('L')
 
 # Resize the image to 260x200 pixels
-new_image = image2.resize((260, 200), Image.ANTIALIAS)
+new_image = image2.resize((2592, 2048), Image.LANCZOS)      # compress the image 
 
 # save image
 new_image.show()
